8c28e5b5e3b08dbcdc3ff6e75b4894ab9dcb2c35
psycho <nitropsycho84@gmail.com>
2025-03-12 12:32:14 +0530

    event creation and special session request

---
+++ INDIACom/Models/EventModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INDIACom.Models
+{
+    public class EventModel
+    {
+        public string Event_Id { get; set; }
+        public string Event_Name { get; set; }
+        public string Event_Description { get; set; }
+        public DateTime Event_Creation_date { get; set; }
+        public DateTime Event_Opening_date { get; set; }
+        public DateTime Event_Closing_date { get; set; }
+        public string Event_Type { get; set; }
+
+
+
+    }
+}
---
+++ INDIACom/Models/SpecialSessionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INDIACom.Models
+{
+    public class SpecialSessionModel
+    {
+
+        public string SSName { get; set; }
+        public int Mobile { get; set; }
+        public string Email { get; set; }
+        public string Organization { get; set; }
+        public string Topic { get; set; }
+        public DateTime Request_Date { get; set; }
+    }
+}
